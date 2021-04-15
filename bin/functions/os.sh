3265e8195caebeda22e048088fd787c67b738a4f
Luis Aveiro <git@luisaveiro.com>
2021-04-15 20:11:42 +0100

    feat: added Linux as supported OS.

--- bin/functions/os.sh
+++ bin/functions/os.sh
@@ -3,7 +3,7 @@
 # Internal os functions.
 
 #######################################
-# Check if operating system is either macOS.
+# Check if operating system is either macOS or Linux.
 #
 # Returns:
 #   1 if OS is not supported.
@@ -14,6 +14,8 @@ function is_operating_system_supported() {
   unameout="$(uname -s)"
 
   case $unameout in
+    Linux*)
+      machine=Linux;;
     Darwin*)
       machine=mac;;
     *)
@@ -22,7 +24,7 @@ function is_operating_system_supported() {
 
   if [ $machine == "UNKNOWN" ]; then
     error "Unsupported OS [$(ansi --bold --white "${unameout}")]." \
-      "$(ansi --italic --bold --white DEV.env) supports macOS."
+      "$(ansi --italic --bold --white DEV.env) supports macOS and Linux."
 
     exit 1
   fi
