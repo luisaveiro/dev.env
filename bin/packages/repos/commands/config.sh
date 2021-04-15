c60bc09a936ae615569fdaeae55e9124b9b0ef11
Luis Aveiro <git@luisaveiro.com>
2021-04-15 23:13:53 +0100

    refactor: `repos:config` command
    - `repos:config` command accepts git repository as a remote configuration.

--- bin/packages/repos/commands/config.sh
+++ bin/packages/repos/commands/config.sh
@@ -3,15 +3,13 @@
 # Repos package config command.
 
 #######################################
-# Add repositories YML file to DEV.env.
-#
-# Globals:
-#   User
+# Add repositories YAML file to DEV.env.
 #
 # Arguments:
 #   --template_dir
+#   --only
 #   --name
-#   config_path
+#   config file
 #
 # Returns:
 #   1 if remote config file missing name or
@@ -21,8 +19,8 @@
 #   config file does not exist.
 #######################################
 function repos::config() {
-  local arguments_list=("template_dir" "name")
-  local template_dir name config_file=$*
+  local arguments_list=("template_dir" "only" "name")
+  local template_dir only name config_file=$*
 
   while [ $# -gt 0 ]; do
     if [[ $1 == *"--"* && $1 == *"="* ]]; then
@@ -41,83 +39,15 @@ function repos::config() {
   # Strip out whitespaces
   config_file="${config_file//[[:blank:]]/}"
 
-  if [[ -z ${name} ]] && is_file_remote "${config_file}"; then
-    error --newline=bottom "You need to include the" \
-      "$(ansi --bold --white --name) option for remote based repositories" \
-      "configuration files."
-
-    info --newline=bottom "Update your command input with the follows:"
-
-    output "$(ansi --bold --white dev repos:config "${config_file}")" \
-      "$(ansi --bold --white --inverse --name="${USER:-personal}")"
-
-    exit 1;
-  elif ! is_file_remote "${config_file}"; then
-    local file_name
-    file_name="$(basename -- "${config_file}")"
-
-    if [ "${file_name: -4}" != ".yml" ]; then
-      error "Repositories configuration file must be a YAML file."
-
-      exit 1
-    fi
-
-    if [[ -z ${name} ]]; then
-      name="${file_name}"
-    fi
-  fi
-
-  if [ "${name: -4}" != ".yml" ]; then
-    name="${name}.yml"
-  fi
-
-  if [[ $name == "repositories-template.yml" ]]; then
-    error "$(ansi --bold --white "${name}")" \
-      "is a reserved configuration template!" \
-      "Please select a different configuration name."
-
-    exit 1
-  fi
-
-  # check if file already exists.
-  if file_exists "${template_dir}/${name}"; then
-    warning "$(ansi --bold --white "${name}")" \
-      "already exists as a repositories configuration file."
-
-    if ! ask "Do you want to override the existing repositories configuration file?";
-    then
-      exit 1
-    fi
-  fi
-
-  if is_file_remote "${config_file}"; then
-    local gist_hash gist_dir
-    gist_hash="$(git::gist_hash "${config_file}")"
-    gist_dir="${template_dir}/${gist_hash}"
-
-    if ! git::clone --quiet --git="${config_file}" --dir="${gist_dir}"; then
-      error "Gist hash not found. Could not read from remote repository."
-      output "Please make sure you have the correct access rights" \
-        "and the gist hash exists."
-    fi
-
-    mv "${gist_dir}/${name}" "${template_dir}/${name}"
-    rm -rf "${gist_dir:?}"
+  if ! is_file_remote "${config_file}"; then
+    repos::local_configuration \
+      --template_dir="${template_dir}" \
+      --name="${name}" \
+      "${config_file}"
   else
-    if ! file_exists "${config_file}"; then
-      error "Unable to locate file" \
-        "$(ansi --bold --white "$(file_name "${config_file}")")."
-      info "Please check" \
-        "$(ansi --bold --white "$(file_name "${config_file}")")" \
-        "exists and file path" \
-        "($(ansi --bold --white "$( dirname "${config_file}")")) is correct."
-
-      exit 1
-    fi
-
-    cp "${config_file}" "${template_dir}/${name}"
+    repos::remote_configuration \
+      --template_dir="${template_dir}" \
+      --only="${only}" \
+      "${config_file}"
   fi
-
-  info --newline=top "Saved $(ansi --bold --white "${name}") in" \
-    "($(ansi --bold --white "${template_dir}")) directory."
 }
