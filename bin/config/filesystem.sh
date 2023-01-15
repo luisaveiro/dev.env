11e5c7f8355339831b7a8c4e8fdf54ada546934d
Luis Carlos Aveiro <git@luisaveiro.com>
2023-01-15 11:38:37 +0000

    feat: added TEMPLATE_DIR in filesystem config.

--- bin/config/filesystem.sh
+++ bin/config/filesystem.sh
@@ -9,3 +9,10 @@
 PROJECT_DIR="$( dirname "${BIN_DIR}" )"
 readonly PROJECT_DIR
 export PROJECT_DIR
+
+#######################################
+# Template Directory
+#######################################
+
+readonly TEMPLATE_DIR="${PROJECT_DIR}/templates"
+export TEMPLATE_DIR
