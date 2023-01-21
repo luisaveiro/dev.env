647cc6a9eab66df64aaa31fdecb6c7729535e335
Luis Carlos Aveiro <git@luisaveiro.com>
2023-01-21 11:14:13 +0000

    feat: added REPOS_TEMPLATE_YAML in repos config.

--- bin/config/repos.sh
+++ bin/config/repos.sh
@@ -6,5 +6,12 @@
 # Repos YAML File
 #######################################
 
-readonly REPOS_YAML="repositories.yml"
+readonly REPOS_TEMPLATE_YAML="repositories-template.yaml"
+export REPOS_TEMPLATE_YAML
+
+#######################################
+# Repos YAML File
+#######################################
+
+readonly REPOS_YAML="repositories.yaml"
 export REPOS_YAML
