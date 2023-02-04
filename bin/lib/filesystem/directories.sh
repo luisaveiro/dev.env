d7287e129fc1c07e1066844af13c076ffbb1d414
Luis Carlos Aveiro <git@luisaveiro.com>
2023-02-04 13:27:28 +0000

    feat: added filesystem directory function.

--- bin/lib/filesystem/directories.sh
+++ bin/lib/filesystem/directories.sh
@@ -15,3 +15,17 @@
 function filesystem::does_directory_exists() {
   [ -d "${1}" ] && return 0 || return 1
 }
+
+#######################################
+# Check if the directory is empty.
+#
+# Arguments:
+#   Directory
+#
+# Returns:
+#   0 if the directory is empty.
+#   1 if the directory is not empty.
+#######################################
+function filesystem::is_directory_empty() {
+  [ "$( ls -A "${1}" )" ] && return 0 || return 1
+}
